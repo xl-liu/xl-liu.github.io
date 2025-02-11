a69242abe4b96a9617b890c2f6912cac401a16a2
Xintong Liu <xintongliu211@gmail.com>
2025-02-11 14:47:34 -0500

    updated the homepage icons

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import aboutIcon from './icons/aboutme0.png';
-import aboutIcon1 from './icons/aboutme1.png';
+// import aboutIcon from './icons/aboutme0.png';
+// import aboutIcon1 from './icons/aboutme1.png';
 import eyeIcon from './icons/eye0.png';
 import eyeIcon1 from './icons/eye2.png';
 import mouthIcon from './icons/mouth0.png';
 import mouthIcon1 from './icons/mouth2.png';
+import handIcon from './icons/hand0.png';
+import handIcon1 from './icons/hand1.png';
 import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import About from './components/About';
 import Contact from './components/Contact';
@@ -15,18 +17,18 @@ function NavContent() {
     <div className="App">
       <div className="nav-container">
         <div className="nav-item" onClick={() => navigate('/about')}>
-          <img src={aboutIcon} alt="about" className="icon-default" />
-          <img src={aboutIcon1} alt="about" className="icon-hover" />
+          <img src={eyeIcon} alt="about" className="icon-default" />
+          <img src={eyeIcon1} alt="about" className="icon-hover" />
           <span className="nav-text">about</span>
         </div>
         <div className="nav-item">
-          <img src={eyeIcon} alt="projects" className="icon-default"/>
-          <img src={eyeIcon1} alt="projects" className="icon-hover"/>
+          <img src={mouthIcon} alt="projects" className="icon-default"/>
+          <img src={mouthIcon1} alt="projects" className="icon-hover"/>
           <span className="nav-text">projects</span>
         </div>
         <div className="nav-item" onClick={() => navigate('/contact')}>
-          <img src={mouthIcon} alt="contact" className="icon-default"/>
-          <img src={mouthIcon1} alt="contact" className="icon-hover"/>
+          <img src={handIcon} alt="contact" className="icon-default"/>
+          <img src={handIcon1} alt="contact" className="icon-hover"/>
           <span className="nav-text">contact</span>
         </div>
       </div>
